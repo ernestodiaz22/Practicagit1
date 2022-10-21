13425c94ab9598320e208b83bf2712b63e46c651
Ernesto Díaz Jiménez <ernestodiaz22@ilg.cat>
2022-10-21 13:52:28 +0200

    añadiendo cabacera

--- Program.java
+++ Program.java
@@ -1,4 +1,14 @@
-
+/*
+ * Program.java        20.10.2020
+ *
+ * Program to calculate the area and the perimeter
+ * of a circle knowing the radius in meter.
+ *
+ * Copyright 2020 Ernesto Díaz Jiménez <ernestodiaz22@ilg.cat>
+ *
+ * This is free software, licensed under the GNU General Public License v3.
+ * See http://www.gnu.org/licenses/gpl.ht	ml for more information.
+ */
 package gitTerminal02;
 
 import java.util.Scanner;
